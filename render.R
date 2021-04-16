965b59b405b6c9cd9f01c0cf68230d192a8e4af2
samuel-vianna <65961931+samuel-vianna@users.noreply.github.com>
2021-04-15 21:51:13 -0300

    Update render.R

--- render.R
+++ render.R
@@ -1,2 +1,2 @@
-bookdown::render_book("00-index.Rmd", "bookdown::gitbook")
-bookdown::render_book("00-index.Rmd", "bookdown::pdf_book")
+bookdown::render_book("00-index.Rmd", "bookdown::gitbook", output_dir = "../docs")
+# bookdown::render_book("00-index.Rmd", "bookdown::pdf_book")
